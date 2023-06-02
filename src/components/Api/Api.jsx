036b13d1fc65b16e9932e1c9a62aeebecadffc6d
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-06-02 20:22:27 +0200

    fix

---
+++ src/components/Api/Api.jsx
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '34871467-f144c0dca622e0cd08d0dd1ad';
+
+function fetchQuery(searchQuery, page) {
+  return fetch(
+    `${BASE_URL}/?key=${API_KEY}&q=${searchQuery}
+      &image_type=photo&orientation=horizontal&page=
+      ${page}&per_page=12`
+  ).then(response => response.json());
+}
+
+const api = { fetchQuery };
+
+export default api;
